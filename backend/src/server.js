5785505a55f9c59db03d81d4d9deca7a77078c21
Yohannes Walle <etawe1627@gmail.com>
2024-06-24 03:30:37 -1200

    server listend

---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+require('colors');
+require('dotenv').config();
+const app = express();
+
+
+
+// listening the server
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`.cyan.bold);
+});
