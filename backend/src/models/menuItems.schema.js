3fa5309d9bb4f37f76c2c0a0d25f99c87d8bace1
Yohannes Walle <etawe1627@gmail.com>
2024-07-05 01:59:02 -1200

    menuItems model added

---
+++ backend/src/models/menuItems.schema.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+
+
+const menuItemSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    }, image: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+    },
+    tags: {
+        type: [String],
+    },
+    restaurant: {
+        type: Schema.Types.ObjectId,
+        ref: 'Restaurant',
+        required: true
+    }
+});
+
+const MenuItem = model('MenuItem', menuItemSchema);
+
+module.exports = MenuItem;
