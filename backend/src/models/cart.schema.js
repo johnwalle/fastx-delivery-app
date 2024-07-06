7f0ab2c3aa7ba2972ac8d6a33ff5764339305e6f
Yohannes Walle <etawe1627@gmail.com>
2024-07-06 07:33:51 -1200

    cart model created

--- backend/src/models/cart.schema.js
+++ backend/src/models/cart.schema.js
@@ -8,7 +8,7 @@ const CartSchema = new Schema({
     items: [
         {
             menuItem: { type: Schema.Types.ObjectId, ref: 'Menu', required: true },
-            quantity: { type: Number, required: true, min: 1 },
+            quantity: { type: Number, required: true, min: 1, default: 1 },
             price: { type: Number, required: true }
         }
     ]
