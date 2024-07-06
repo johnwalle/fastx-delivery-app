34fb98d59500466d1ac1d8eaa1a043f9f3f1726e
Yohannes Walle <etawe1627@gmail.com>
2024-07-06 04:17:56 -1200

    review controller added

--- backend/src/controllers/review.controller.js
+++ backend/src/controllers/review.controller.js
@@ -1,12 +1,14 @@
-const User = require('../models/user.schema')
-const Menu = require('../models/menu.schema')
 const catchAsync = require('../utils/catchAsync')
 const reviewService = require('../services/review.service')
-const userService = require('../services/user.service')
+const ApiError = require('../utils/apiError')
+const httpStatus = require('http-status')
+const menuItemService = require('../services/menuItem.service')
 
-// GET /reviews: Get a list of all reviews.
-const getAllReviews = catchAsync(async (req, res) => {
-    const reviews = await reviewService.getAllReviews()
+
+// GET /reviews: Get a list of all reviews of a menu ite,
+const getItemReviews = catchAsync(async (req, res) => {
+    const { itemId } = req.params
+    const reviews = await reviewService.getItemReviews(itemId)
     res.json(reviews)
 })
 
@@ -17,37 +19,63 @@ const getReviewById = catchAsync(async (req, res) => {
 
     const review = await reviewService.getReviewById(req.params.id)
     if (!review) {
-        return res.status(404).json({ message: 'Review not found' })
+        throw new ApiError(httpStatus.NOT_FOUND, 'Review not found')
     }
     res.json(review)
 })
 
 
 
-// POST /reviews: Create a new review.
-
+// POST /reviews: Create a new review
 const createReview = catchAsync(async (req, res) => {
 
     const userId = req.user._id.toString();
+    const { rating, comment } = req.body;
+    const { itemId } = req.params;
 
-    const { menuItem, rating, comment } = req.body;
+    // check if the user reviewed before
+
+    const reviewed = await reviewService.getReviewByUserAndItemId(userId, itemId)
+    if (reviewed.length > 0) {
+        return res.status(400).json({ message: 'You have already reviewed this item' })
+    }
+
+
+    // check if the menuitem that the user trying to review exists
+
+    const menuItem = await menuItemService.getMenuItemById(itemId)
+    if (!menuItem) {
+        return res.status(404).json({ message: 'Menu item not found' })
+    }
+
+    // check if all the fields are filled
+
+    if (!rating || !comment) {
+        return res.status(400).json({ message: 'please fill all the fields' })
+    }
+
+
 
     const review = {
         user: userId,
-        menuItem,
+        menuItem: itemId,
         rating,
         comment
     }
+    const created = await reviewService.createReview(review)
+    // update the rating attribute in the menuitem
 
-    const newReview = await reviewService.createReview(review)
-    res.status(201).json(newReview)
+    if (created) {
+        await menuItemService.updateRating(itemId);
+        res.status(201).json({ success: true, message: 'Review created successfully' })
+    }
+
 
 })
 
 
 
-// PUT /reviews/:id: Update a specific review by its ID.
-
+// PUT /reviews/:id: Update a specific review by its ID
 const updateReview = catchAsync(async (req, res) => {
 
     const userId = req.user._id.toString();
@@ -61,24 +89,28 @@ const updateReview = catchAsync(async (req, res) => {
         return res.status(403).json({ message: 'You are not authorized to update this review' })
     }
 
-    const { menuItem, rating, comment } = req.body;
+    const { rating, comment } = req.body;
 
     const updateReview = {
-        menuItem,
         rating,
         comment
     }
+    const updatedReview = await reviewService.updateReview(req.params.id, updateReview)
 
-    const updatedReview = await reviewService.updateReview(updateReview)
-    res.json(updatedReview)
+    // update the rating attribute in the menuitem
 
+    await menuItemService.updateRating(review.menuItem);
+    res.status(200).json({
+        success: true,
+        message: 'Review updated successfully',
+        updatedReview
+    })
 });
 
 
 
 
-// DELETE /reviews/:id: Delete a specific review by its ID.
-
+// DELETE /reviews/:id: Delete a specific review by its ID
 const deleteReview = catchAsync(async (req, res) => {
 
     const userId = req.user._id.toString();
@@ -93,13 +125,15 @@ const deleteReview = catchAsync(async (req, res) => {
     }
 
     await reviewService.deleteReview(req.params.id)
-    res.status(204).end()
+    // update the rating attribute in the menuitem
+    await menuItemService.updateRating(review.menuItem);
 
+    res.json({ success: true, message: 'Review deleted successfully' })
 });
 
 
 module.exports = {
-    getAllReviews,
+    getItemReviews,
     getReviewById,
     createReview,
     updateReview,
