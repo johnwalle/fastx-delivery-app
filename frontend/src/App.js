5104da58b8ffa71749a8f12508ee0643c91c3d07
Yohannes Walle <etawe1627@gmail.com>
2024-08-27 15:42:57 -1200

    navbar removed from login and register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,46 @@
-// Filename: App.js
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/navbar'
-import LandingPage from './pages/LandingPage'
-import LoginPage from './pages/LoginPage'
-import SignupPage from './pages/SignupPage'
-import ForgotPasswordPage from './pages/ForgotPasswordPage'
-import Footer from './components/footer'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Navbar from './components/navbar';
+import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import ForgotPasswordPage from './pages/ForgotPasswordPage';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
-        <Routes>
-          {/* Landing Page */}
-          <Route path='/' element={<LandingPage />} />
-
-          {/* Login Page */}
-          <Route path='/login' element={<LoginPage />} />
-
-          {/* Signup Page */}
-          <Route path='/signup' element={<SignupPage />} />
-
-          {/* Forgot Password Page */}
-          <Route path='/forgot' element={<ForgotPasswordPage />} />
-        </Routes>
-        <Footer />
+        <MainContent />
       </Router>
     </div>
+  );
+}
+
+// Component to handle conditional rendering of Footer
+function MainContent() {
+  const location = useLocation();
+
+  // Check if the current path is '/login' or '/signup'
+  const shouldHideFooter = ['/login', '/signup'].includes(location.pathname);
+
+  return (
+    <>
+      <Routes>
+        {/* Landing Page */}
+        <Route path='/' element={<LandingPage />} />
+
+        {/* Login Page */}
+        <Route path='/login' element={<LoginPage />} />
+
+        {/* Signup Page */}
+        <Route path='/signup' element={<SignupPage />} />
+
+        {/* Forgot Password Page */}
+        <Route path='/forgot' element={<ForgotPasswordPage />} />
+      </Routes>
+      {!shouldHideFooter && <Footer />}
+    </>
   );
 }
 
