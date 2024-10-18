8fa63e555af3d2af6e0e4d24fa7202f1128940aa
Yared3214 <yaredman0099@gmail.com>
2024-10-18 00:04:56 -0700

    SuperAdmin page added

--- frontend/src/pages/SuperAdminPage/index.js
+++ frontend/src/pages/SuperAdminPage/index.js
@@ -1,9 +1,171 @@
-import React from 'react'
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { createTheme } from '@mui/material/styles';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { AppProvider } from '@toolpad/core/AppProvider';
+import { DashboardLayout } from '@toolpad/core/DashboardLayout';
+import { ChevronDown, Utensils } from 'lucide-react';
+import fastXLogo from '../../assets/fastX-logo.png';
+import CreateRestaurantPage from '../CreateRestaurantPage';
+import { Accordion, AccordionActions, AccordionDetails, AccordionSummary, Button } from '@mui/material';
 
-function SuperAdminPage() {
+
+const NAVIGATION = [
+    {
+        segment: 'order',
+        title: 'Manage Orders',
+        icon: <ShoppingCartIcon />,
+    },
+    {
+        segment: 'restaurant',
+        title: 'Add Restaurants',
+        icon: <Utensils />,
+    },
+];
+
+const customTheme = createTheme({
+    palette: {
+        background: {
+            default: '#F9F9FE',
+            paper: '#A40C0C',
+        },
+        text: {
+            primary: '#000000',
+            secondary: '#555555',
+        },
+        action: {
+            active: '#ffffff',
+            hover: '#a72828',
+            selected: '#FF6347',
+            disabled: '#BDBDBD',
+        },
+    },
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 600,
+            md: 600,
+            lg: 1200,
+            xl: 1536,
+        },
+    },
+});
+
+function DemoPageContent({ pathname }) {
     return (
-        <div>SuperAdminPage</div>
-    )
+        <Box
+            sx={{
+                py: 4, px: 3
+            }}
+        >
+            {pathname === '/order' ?
+                <Typography>
+                    <div>
+                        <Accordion>
+                            <AccordionSummary
+                                expandIcon={<ChevronDown />}
+                                aria-controls="panel1-content"
+                                id="panel1-header"
+                            >
+                                Accordion 1
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                                malesuada lacus ex, sit amet blandit leo lobortis eget.
+                            </AccordionDetails>
+                        </Accordion>
+                        <Accordion>
+                            <AccordionSummary
+                                expandIcon={<ChevronDown />}
+                                aria-controls="panel2-content"
+                                id="panel2-header"
+                            >
+                                Accordion 2
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                                malesuada lacus ex, sit amet blandit leo lobortis eget.
+                            </AccordionDetails>
+                        </Accordion>
+                        <Accordion defaultExpanded>
+                            <AccordionSummary
+                                expandIcon={<ChevronDown />}
+                                aria-controls="panel3-content"
+                                id="panel3-header"
+                            >
+                                Accordion Actions
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                                malesuada lacus ex, sit amet blandit leo lobortis eget.
+                            </AccordionDetails>
+                            <AccordionActions>
+                                <Button>Cancel</Button>
+                                <Button>Agree</Button>
+                            </AccordionActions>
+                        </Accordion>
+                    </div>
+                </Typography> :
+                <Typography>
+                    <CreateRestaurantPage />
+                </Typography>}
+        </Box>
+    );
 }
 
-export default SuperAdminPage
+DemoPageContent.propTypes = {
+    pathname: PropTypes.string.isRequired,
+};
+
+function DashboardLayoutBranding(props) {
+    const { window } = props;
+
+    const [pathname, setPathname] = React.useState('/dashboard');
+
+    const router = React.useMemo(() => {
+        return {
+            pathname,
+            searchParams: new URLSearchParams(),
+            navigate: (path) => setPathname(String(path)),
+        };
+    }, [pathname]);
+
+    // Remove this const when copying and pasting into your project.
+    const demoWindow = window !== undefined ? window() : undefined;
+
+    return (
+        // preview-start
+        <AppProvider
+            navigation={NAVIGATION}
+            branding={{
+                logo: (
+                    <a href="/" aria-label="Home">
+                        <img src={fastXLogo} alt="MUI logo" />
+                    </a>
+                ),
+                title: <span className="text-white font-bold">fastX</span>,
+            }}
+            router={router}
+            theme={customTheme}
+            window={demoWindow}
+        >
+            <DashboardLayout>
+                <DemoPageContent pathname={pathname} />
+            </DashboardLayout>
+        </AppProvider>
+        // preview-end
+    );
+}
+
+DashboardLayoutBranding.propTypes = {
+    /**
+     * Injected by the documentation to work in an iframe.
+     * Remove this when copying and pasting into your project.
+     */
+    window: PropTypes.func,
+};
+
+export default DashboardLayoutBranding;
